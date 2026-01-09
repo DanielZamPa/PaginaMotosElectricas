038e194ae6fc5b63d1c06641df4dafe0a2b658c6
daniel.uzumakifutoon <daniel.uzumakifutoon@gmail.com>
2026-01-09 20:01:35 +0000

    Primeros pasos de CSS, fondo obtenido de UIVERSE

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-// Add JS here
-import { Tranquiluxe } from "uvcanvas"
+document.addEventListener("DOMContentLoaded", function() {
+    const lightThemeButton = document.getElementById("light-theme");
+    const darkThemeButton = document.getElementById("dark-theme");
+
+    lightThemeButton.addEventListener("click", () => {
+        document.body.classList.add("light-theme");
+        document.body.classList.remove("dark-theme");
+    });
+
+    darkThemeButton.addEventListener("click", () => {
+        document.body.classList.add("dark-theme");
+        document.body.classList.remove("light-theme");
+    });
+});
